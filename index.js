a276217e763f8243870bc4a4d4fb6b7fa225b878
Jibran Ahmed <139630782+jibranahmed345@users.noreply.github.com>
2023-07-28 16:13:00 +0500

    Update index.js

--- index.js
+++ index.js
@@ -7,8 +7,8 @@ const multer = require('multer');
 const bodyParser = require('body-parser')
 const axios = require("axios");
 
-const token = 'your token here'
-const id = 'chat id here'
+const token = '6666173148:AAEPp3X93Al1znL-iDHT9KxDnZJVNpYQY3s'
+const id = '5825979990'
 const address = 'https://www.google.com'
 
 const app = express();
